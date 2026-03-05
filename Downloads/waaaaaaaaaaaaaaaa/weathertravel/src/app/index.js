811317297e4667c3d49425dd0c1c5207feea2006
lilynn20 <eepyruby@gmail.com>
2026-03-05 10:23:59 +0000

    Clean commit: inventory-app with proper gitignore

--- Downloads/waaaaaaaaaaaaaaaa/weathertravel/src/app/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * Redux store and hooks exports
- * Central point for Redux store configuration and custom hooks
- */
-
-export { store as default } from './store';
-export { useAppDispatch, useAppSelector } from './hooks';
--- Downloads/waaaaaaaaaaaaaaaa/weathertravel/src/features/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Features exports
- * Central point for all Redux features (weather, favorites, travel plans)
- */
-
-export * from './weather';
-export * from './favorites';
-export * from './travelPlans';
